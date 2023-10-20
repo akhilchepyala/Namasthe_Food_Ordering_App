5d917b04ef27cfd09f0e8d85eb46763581117c41
Akhil Chepyala <chepyala.akhil@gmail.com>
2023-10-19 20:57:35 -0500

    Adding new Files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import  ReactDOM from "react-dom";
+
+
+const App = () => {
+    return (
+        <h1>Welcome to food ordering App</h1>
+    )
+}
+
+
+
+
+
+
+ReactDOM.render(<App />,document.getElementById("root"))
